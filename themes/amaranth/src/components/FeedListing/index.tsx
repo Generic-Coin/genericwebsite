79484e08f94502eece9503dff971c31a1213be07
Joel Cuthriell <joel@cuthriell.com>
2022-03-29 11:07:37 -0500

    updates

--- themes/amaranth/src/components/FeedListing/index.tsx
+++ themes/amaranth/src/components/FeedListing/index.tsx
@@ -3,40 +3,33 @@ import styled from "styled-components";
 
 import { Types } from "gatsby-theme-advanced";
 
-import ArticleCard from "../ArticleCard";
 import { constants } from "../../theme";
 import LayoutWidthContainer from "../shared/LayoutWidthContainer";
 
-import GenericSizzle from "./genericday.mp4"
-import GenericPoster from "./play.jpg"
+import GenericSizzle from "./genericday.mp4";
+import GenericPoster from "./play.jpg";
 
-const Wrapper = styled.div`
-  width: 100%;
-
-  display: block;
-  grid-gap: 80px;
-  grid-template-columns: 1fr 1fr;
-
-  @media (max-width: ${constants.breakpoints.lg}) {
-    grid-template-columns: 1fr;
-    gap: 60px;
+const Contract = styled.div`
+  width: 80%;
+  margin: 2rem 0;
+  marquee {
+    width: 100%;
   }
 `;
 
 const Tokenomics = styled.div`
   width: 100%;
-
   display: block;
   grid-gap: 80px;
   grid-template-columns: 1fr 1fr;
 
   table {
-    min-width: 30rem;
-  }
-  .main-table {
-    margin: 2rem 0;
+    margin: 1rem 0;
+    width: 100%;
+    max-width: 30rem;
+
     td {
-      padding: .3rem;
+      padding: 0.25rem;
     }
   }
 
@@ -46,22 +39,30 @@ const Tokenomics = styled.div`
   }
 `;
 
-const Contract = styled.div`
-  width: 80%;
+const Presale = styled.div`
   margin: 2rem 0;
-  marquee {
-    width:100%;
-  }
-`
+`;
+
+const Team = styled.div`
+  margin: 2rem 0;
+`;
+
+const Partnerships = styled.div`
+  margin: 2rem 0;
+`;
 
 const Videobox = styled.div`
-  margin-top: 2rem;
-  width:100%;
-  max-width:25rem;
+  margin-top: 1rem;
+  width: 100%;
+  max-width: 25rem;
   video {
-    width:100%;
+    width: 100%;
   }
-`
+`;
+
+const Footer = styled.div`
+  margin: 2rem 0;
+`;
 
 const WidthLimitedGrid = styled(LayoutWidthContainer)`
   display: block;
@@ -78,13 +79,20 @@ type PostListingProps = {
 
 const FeedListing = ({ listing, noHero }: PostListingProps): JSX.Element => (
   <WidthLimitedGrid>
-    <p>Generic Coin is a Binance Smart Chain project that focuses on features that users find important in today's evolving crypto landscape.<br />
-    You can buy Generic Coin<br />
-    You can sell Generic Coin<br />
-    You can send Generic Coin<br />
-    Have a Generic Day! - The Generic Coin Team.</p>
+    <p>
+      Generic Coin is a Binance Smart Chain project that focuses on features
+      that users find important in today's evolving crypto landscape.
+      <br />
+      You can buy Generic Coin
+      <br />
+      You can sell Generic Coin
+      <br />
+      You can send Generic Coin
+      <br />
+      Have a Generic Day! - The Generic Coin Team.
+    </p>
     <br />
-    <Wrapper>
+    {/* <Wrapper>
       {listing.map((feedItem, idx) => {
         // Check if we're rendering a placeholder
         if ("turnedoff_isPlaceholder" in feedItem)
@@ -96,81 +104,146 @@ const FeedListing = ({ listing, noHero }: PostListingProps): JSX.Element => (
           <ArticleCard key={feedItem.slug} post={feedItem} />
         );
       })}
-    </Wrapper>
+    </Wrapper> */}
+
+    <Videobox>
+      <video poster={GenericPoster} controls>
+        <source src={GenericSizzle} type="video/mp4" />
+      </video>
+    </Videobox>
 
     <Contract>
-      <h4>Generic Contract</h4>
-      <marquee width="100%" direction="left" height="30px">
-        <a href="https://bscscan.com/token/0x0E121672dCe41034598ba1D71AA958c4Eb6C4DF3" target="_blank">0x0E121672dCe41034598ba1D71AA958c4Eb6C4DF3</a>
-      </marquee>
+      <a
+        href="https://bscscan.com/token/0x0E121672dCe41034598ba1D71AA958c4Eb6C4DF3"
+        target="_blank"
+      >
+        <h4>Generic Contract</h4>
+        <marquee width="100%" direction="left" height="30px">
+          0x0E121672dCe41034598ba1D71AA958c4Eb6C4DF3
+        </marquee>
+      </a>
     </Contract>
 
     <Tokenomics>
-    <table className="main-table" border="2" cellpadding="2" cellspacing="2">
-      <thead>
-        <tr>
-          <td><h4>Generic Tokenomics</h4></td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <table border="2" cellpadding="2" cellspacing="2">
-              <thead>
-                <tr>
-                  <td>Buy</td>
-                  <td>Sell</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1% team/marketing tax</td>
-                  <td>1% team/marketing tax<br />
-                      4% locked LP tax</td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <table border="2" cellpadding="2" cellspacing="2">
-              <thead>
-                <tr>
-                  <td>1,000,000,000 total supply </td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>88.3% of total supply for presale</td>
-                </tr>
-                <tr>
-                  <td>44.5% of the total supply for presale</td>
-                </tr>
-                <tr>
-                  <td>
-                    43.8% of total supply for liquidity<br />
-                    (100% lock for 1 year)
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
+      <h4>Generic Tokenomics</h4>
+      <table border="2">
+        <thead>
+          <tr>
+            <td>Buy</td>
+            <td>Sell</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1% Team/Marketing Tax</td>
+            <td>
+              1% Team/Marketing Tax
+              <br />
+              4% Locked LP Tax
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table border="2">
+        <thead>
+          <tr>
+            <td>1,000,000,000,000 Total Supply</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>44.5% of the Total Supply for Presale</td>
+          </tr>
+          <tr>
+            <td>
+              43.8% of Total Supply for Liquidity (100% Locked for 1 Year)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              11% of Total Supply for Private Sale to Raise BUSD to be used for
+              Buyback and Burns to Correct Dumps
+            </td>
+          </tr>
+          <tr>
+            <td>0.7% of Total Supply for Apeswap's IAO Listing Fee</td>
+          </tr>
+        </tbody>
+      </table>
     </Tokenomics>
-    
-    <Videobox >
-    <video 
-      poster={GenericPoster}
-      controls
-    >
-      <source src={GenericSizzle} 
-              type="video/mp4" />
-    </video>
-    </Videobox>
+
+    <Team>
+      <h4>Generic Team</h4>
+      <p>
+        <a
+          href="mailto:genericcoin@outlook.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          James Smith - Generic CEO
+        </a>
+      </p>
+      <p>
+        <a href="https://t.me/Hatmonke" target="_blank" rel="noreferrer">
+          Hat Monke - Developer
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://t.me/fivemilesbeneathcariboucoffee"
+          target="_blank"
+          rel="noreferrer"
+        >
+          caribou - UI / UX
+        </a>
+      </p>
+      <p>
+        <a href="https://t.me/Mrdoodley" target="_blank" rel="noreferrer">
+          Charlie Doodle - Designer
+        </a>
+      </p>
+    </Team>
+
+    <Partnerships>
+      <h4>Generic Partnerships</h4>
+      <p>
+        <a href="https://t.me/IslandShibsChat" target="_blank" rel="noreferrer">
+          Island Shibs - t.me/IslandShibsChat
+        </a>
+      </p>
+      <p>
+        <a href="https://t.me/partyhat" target="_blank" rel="noreferrer">
+          Partyhat - t.me/partyhat
+        </a>
+      </p>
+    </Partnerships>
+
+    <Footer>
+      <br />
+      <br />
+      <h4>Generic Contacts</h4>
+      <p>
+        <a
+          href="mailto:genericcoin@outlook.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          genericcoin@outlook.com
+        </a>
+        <br />
+        <a
+          href="https://twitter.com/thegenericcoin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          twitter.com/TheGenericCoin
+        </a>
+        <br />
+        <a href="https://t.me/genericcoin" target="_blank" rel="noreferrer">
+          t.me/genericcoin
+        </a>
+      </p>
+    </Footer>
   </WidthLimitedGrid>
 );
 
