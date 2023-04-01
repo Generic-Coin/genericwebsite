8029ae441c210aeea5e858e213bea34fcc0cf7fc
Joel Cuthriell <joel@cuthriell.com>
2023-04-01 10:29:55 -0500

    adding ui

--- genericmoney/src/StakingScreen.tsx
+++ genericmoney/src/StakingScreen.tsx
@@ -21,6 +21,7 @@ import {
     useWalletConnect,
     withWalletConnect,
 } from '@walletconnect/react-native-dapp';
+import GenericLogo from './assets/images/gcp.png';
 import { useWeb3React } from '@web3-react/core';
 import Web3 from 'web3';
 import { injected } from './supportedNetworks';
@@ -186,7 +187,43 @@ const StakingScreen = () => {
     }
 
     return (
-        <div>
+        <View style={styles.background}>
+          <View style={styles.container}>
+            <AppBar style={styles.header}>
+              <View style={styles.logo}>
+                <Image style={styles.logoImage} source={GenericLogo} />
+                <Text style={styles.heading} bold disabled>
+                  Generic Coin App
+                </Text>
+              </View>
+              <Button
+                square
+                variant='raised'
+                size='lg'
+                style={styles.aboutButton}
+                onPress={() => openLink('/')}
+              >
+                <Image
+                  style={styles.questionMark}
+                  source={{
+                    uri:
+                      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAFVBMVEUAAACAgID///8AAADAwMCAAAD/AADqeraFAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAHdElNRQflAQwXHQ1lXxPNAAAAqElEQVQoz5WOMQ6DMAxFLUXda3yCGLrTwAmC2CtV5QJVuf8RmmCCTaQOtX6Wp+cfA5aBfRpuJQdAkhyASPLToGGQ/FuKVWnTVaWB/R3xakoDUxcmU0rsw9T15lJV9m9VKZc2zLyBXNr6wAqyHwkV5NLRp1OMgRSZC5DSUYGMixUAUeaHKhuZ36q4IW0tH7OUtm7r+jTAxWU9GfA6CwC1AJdKSDt9BVx6XzBwJ8Kxeb3/AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTAxLTEyVDIzOjI5OjEzKzAwOjAwyc9MIQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wMS0xMlQyMzoyOToxMyswMDowMLiS9J0AAAAASUVORK5CYII=',
+                  }}
+                />
+              </Button>
+            </AppBar>
+        
+            <Panel variant='raised' style={styles.panel}>
+              <Panel variant='cutout' background='canvas' style={styles.cutout}>
+                <ScrollView
+                  style={styles.scrollView}
+                  scrollViewProps={{
+                    contentContainerStyle: styles.content,
+                  }}
+                  alwaysShowScrollbars
+                >
+        
+                    <div>
             <div style={{ width: '100%', display: 'flex' }}>
                 <div style={{ float: 'left', margin: '.75rem 0' }}>
                     <Text
@@ -197,7 +234,7 @@ const StakingScreen = () => {
                             marginBottom: 24,
                         }}
                     >
-                        Generic Slots Beta 0.8
+                        Generic Staking
                     </Text>
                 </div>
             </div>
@@ -219,6 +256,7 @@ const StakingScreen = () => {
             )}
             {active ? (
                 <div>
+                    <Text style={[styles.textInput]}>
                     {tokenBalance ? (<p>Your GENv3 Balance: {tokenBalance} GEN</p>) : (<p></p>)}
                     {stakingTokenBalance ? (<p>Stake token balance: {stakingTokenBalance} LP tokens</p>) : (<p></p>)}
                     {stakingAmount ? (<p>Staked amount: {stakingAmount} LP tokens</p>) : (<p></p>)}
@@ -235,16 +273,136 @@ const StakingScreen = () => {
                     }
                     <Button primary onPress={() => withdraw()}>Witdraw stake</Button>
                     <Button primary onPress={() => claimRewards()}>Claim rewards</Button>
+                    </Text>
                 </div>
             ) : (<></>)}
         </div>
+        
+                </ScrollView>
+              </Panel>
+            </Panel>
+          </View>
+        </View>
     );
 };
 
-export default withWalletConnect(StakingScreen, {
-    redirectUrl:
-        Platform.OS === 'web' ? window.location.origin : 'yourappscheme://',
-    storageOptions: {
-        asyncStorage: AsyncStorage,
-    },
+const styles = StyleSheet.create({
+  textInput: {
+    input: {
+        fontSize: '3rem',
+    }
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  infoView: {
+    maxWidth: '40rem',
+    width: '100%',
+    margin: 'auto',
+  },
+  background: {
+    flex: 1,
+    backgroundColor: '#008080',
+  },
+  container: {
+    flex: 1,
+    maxWidth: '60rem',
+    minWidth: '20rem',
+    width: '100%',
+    margin: 'auto',
+  },
+  textIndent: {
+    paddingLeft: 16,
+  },
+  listItem: {
+    height: 40,
+    paddingHorizontal: 18,
+  },
+  panel: {
+    flex: 1,
+    padding: 8,
+    marginTop: -4,
+    paddingTop: 12,
+  },
+  zpanel: {
+    flex: 1,
+    padding: 8,
+    marginTop: -4,
+    paddingTop: 12,
+    paddingBottom: 100,
+    marginBottom: 18,
+  },
+  slotpanel: {
+    flex: 1,
+    padding: 8,
+    marginTop: -4,
+    paddingTop: 12,
+    paddingBottom: 100,
+    marginBottom: 18,
+    minHeight: '76.6vw',
+  },
+  cutout: {
+    flexGrow: 1,
+    marginTop: 8,
+  },
+  content: {
+    padding: 16,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  section: {
+    marginBottom: 16,
+  },
+  statusBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+
+    marginTop: 4,
+  },
+  statusBarItem: {
+    paddingHorizontal: 6,
+    height: 32,
+    justifyContent: 'center',
+  },
+  header: {
+    justifyContent: 'center',
+    marginBottom: -4,
+    zIndex: 10,
+  },
+  logo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 2,
+  },
+  logoImage: {
+    position: 'absolute',
+    left: -38,
+    top: -4,
+    height: 32,
+    width: 32,
+    resizeMode: 'cover',
+  },
+  heading: {
+    fontSize: 24,
+    fontStyle: 'italic',
+  },
+  aboutButton: {
+    position: 'absolute',
+    right: 8,
+    height: 40,
+    width: 40,
+  },
+  questionMark: {
+    width: 26,
+    height: 26,
+  },
+  scrollPanel: {
+    zIndex: -1,
+  },
 });
+
+export default StakingScreen;
+
