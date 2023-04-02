37e7500b0df81d441724c3561eb360126773fcb2
GenericDev1 <genericdev@mail.com>
2023-04-02 19:21:10 +0200

    Spelling fix

--- genericmoney/src/StakingScreen.tsx
+++ genericmoney/src/StakingScreen.tsx
@@ -29,7 +29,7 @@ import stakingABI from './assets/contracts/stakingABI.json';
 import stakingTokenABI from './assets/contracts/stakingTokenABI.json';
 import ADDRESSES from './constants/addresses';
 import ConnectMetamask from './components/ConnectMetamask';
-import type {Contract} from 'web3-eth-contract';
+import type { Contract } from 'web3-eth-contract';
 
 const StakingScreen = () => {
     // Web3 implementation
@@ -37,15 +37,15 @@ const StakingScreen = () => {
     const { active, account, chainId } = useWeb3React();
 
     var tokenContractAddress = ADDRESSES['97'].genericToken;
-       var tokenContract = new web3.eth.Contract(tokenABI, tokenContractAddress);
+    var tokenContract = new web3.eth.Contract(tokenABI, tokenContractAddress);
 
-        // LP Staking contract
-      var  stakingContractAddress = ADDRESSES['97'].staking;
-      var  stakingContract = new web3.eth.Contract(stakingABI, stakingContractAddress);
+    // LP Staking contract
+    var stakingContractAddress = ADDRESSES['97'].staking;
+    var stakingContract = new web3.eth.Contract(stakingABI, stakingContractAddress);
 
-        // The LP token
-       var stakingTokenContractAddress = ADDRESSES['97'].stakingToken;
-       var stakingTokenContract = new web3.eth.Contract(stakingTokenABI, stakingTokenContractAddress);
+    // The LP token
+    var stakingTokenContractAddress = ADDRESSES['97'].stakingToken;
+    var stakingTokenContract = new web3.eth.Contract(stakingTokenABI, stakingTokenContractAddress);
 
     //var selectedAccount = '';
 
@@ -130,24 +130,24 @@ const StakingScreen = () => {
     };
 
     const handleApprove = async () => {
-            try {
-                await stakingTokenContract.methods
-                    .approve(
-                        ADDRESSES['97'].staking,
-                        web3.utils.toWei(depositAmount, 'ether'),
-                    )
-                    .send({ from: account });
-                setHasAllowance(true);
-            } catch (ex) {
-                return;
-            }
+        try {
+            await stakingTokenContract.methods
+                .approve(
+                    ADDRESSES['97'].staking,
+                    web3.utils.toWei(depositAmount, 'ether'),
+                )
+                .send({ from: account });
+            setHasAllowance(true);
+        } catch (ex) {
+            return;
+        }
     };
 
     const handleDepositAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setDepositAmount(e.target.value);
         var trimmed = String(e.target.value).trim();
 
-        if(trimmed !== ''){
+        if (trimmed !== '') {
 
             const hasAllowance = web3.utils.toBN(allowance).gte(web3.utils.toBN(web3.utils.toWei(trimmed, 'ether')));
             setHasAllowance(hasAllowance);
@@ -156,205 +156,205 @@ const StakingScreen = () => {
 
     return (
         <View style={styles.background}>
-          <View style={styles.container}>
-            <AppBar style={styles.header}>
-              <View style={styles.logo}>
-                <Image style={styles.logoImage} source={GenericLogo} />
-                <Text style={styles.heading} bold disabled>
-                  Generic Coin App
-                </Text>
-              </View>
-              <Button
-                square
-                variant='raised'
-                size='lg'
-                style={styles.aboutButton}
-                onPress={() => openLink('/')}
-              >
-                <Image
-                  style={styles.questionMark}
-                  source={{
-                    uri:
-                      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAFVBMVEUAAACAgID///8AAADAwMCAAAD/AADqeraFAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAHdElNRQflAQwXHQ1lXxPNAAAAqElEQVQoz5WOMQ6DMAxFLUXda3yCGLrTwAmC2CtV5QJVuf8RmmCCTaQOtX6Wp+cfA5aBfRpuJQdAkhyASPLToGGQ/FuKVWnTVaWB/R3xakoDUxcmU0rsw9T15lJV9m9VKZc2zLyBXNr6wAqyHwkV5NLRp1OMgRSZC5DSUYGMixUAUeaHKhuZ36q4IW0tH7OUtm7r+jTAxWU9GfA6CwC1AJdKSDt9BVx6XzBwJ8Kxeb3/AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTAxLTEyVDIzOjI5OjEzKzAwOjAwyc9MIQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wMS0xMlQyMzoyOToxMyswMDowMLiS9J0AAAAASUVORK5CYII=',
-                  }}
-                />
-              </Button>
-            </AppBar>
-        
-            <Panel variant='raised' style={styles.panel}>
-              <Panel variant='cutout' background='canvas' style={styles.cutout}>
-                <ScrollView
-                  style={styles.scrollView}
-                  scrollViewProps={{
-                    contentContainerStyle: styles.content,
-                  }}
-                  alwaysShowScrollbars
-                >
-        
-                    <div>
-            <div style={{ width: '100%', display: 'flex' }}>
-                <div style={{ float: 'left', margin: '.75rem 0' }}>
-                    <Text
-                        bold
-                        style={{
-                            fontSize: 22,
-                            margin: 12,
-                            marginBottom: 24,
-                        }}
+            <View style={styles.container}>
+                <AppBar style={styles.header}>
+                    <View style={styles.logo}>
+                        <Image style={styles.logoImage} source={GenericLogo} />
+                        <Text style={styles.heading} bold disabled>
+                            Generic Coin App
+                        </Text>
+                    </View>
+                    <Button
+                        square
+                        variant='raised'
+                        size='lg'
+                        style={styles.aboutButton}
+                        onPress={() => openLink('/')}
                     >
-                        Generic Staking
-                    </Text>
-                </div>
-            </div>
-            <ConnectMetamask />
-            {active ? (
-                <div>
-                    <Text style={[styles.textInput]}>
-                    {tokenBalance ? (<p>Your GENv3 Balance: {tokenBalance} GEN</p>) : (<p></p>)}
-                    {stakingTokenBalance ? (<p>Stake token balance: {stakingTokenBalance} LP tokens</p>) : (<p></p>)}
-                    {stakingAmount ? (<p>Staked amount: {stakingAmount} LP tokens</p>) : (<p></p>)}
-                    {pendingRewards ? (<p>Claimable rewards: {pendingRewards} GEN</p>) : (<p></p>)}
-                    <input value={depositAmount} onChange={handleDepositAmountChange} />
-                    {hasAllowance ? (
-                        <Button primary disabled={!hasAllowance} onPress={() => deposit()}>Deposit</Button>
-                    ) : (
-                        <>
-                            <Button primary disabled={hasAllowance} onPress={() => handleApprove()}>Approve</Button>
-                            <Button primary disabled={hasAllowance} onPress={() => deposit()}>Deposit</Button>
-                        </>
-                    )
-                    }
-                    <Button primary onPress={() => withdraw()}>Witdraw stake</Button>
-                    <Button primary onPress={() => claimRewards()}>Claim rewards</Button>
-                    </Text>
-                </div>
-            ) : (<></>)}
-        </div>
-        
-                </ScrollView>
-              </Panel>
-            </Panel>
-          </View>
+                        <Image
+                            style={styles.questionMark}
+                            source={{
+                                uri:
+                                    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAFVBMVEUAAACAgID///8AAADAwMCAAAD/AADqeraFAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAHdElNRQflAQwXHQ1lXxPNAAAAqElEQVQoz5WOMQ6DMAxFLUXda3yCGLrTwAmC2CtV5QJVuf8RmmCCTaQOtX6Wp+cfA5aBfRpuJQdAkhyASPLToGGQ/FuKVWnTVaWB/R3xakoDUxcmU0rsw9T15lJV9m9VKZc2zLyBXNr6wAqyHwkV5NLRp1OMgRSZC5DSUYGMixUAUeaHKhuZ36q4IW0tH7OUtm7r+jTAxWU9GfA6CwC1AJdKSDt9BVx6XzBwJ8Kxeb3/AAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTAxLTEyVDIzOjI5OjEzKzAwOjAwyc9MIQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wMS0xMlQyMzoyOToxMyswMDowMLiS9J0AAAAASUVORK5CYII=',
+                            }}
+                        />
+                    </Button>
+                </AppBar>
+
+                <Panel variant='raised' style={styles.panel}>
+                    <Panel variant='cutout' background='canvas' style={styles.cutout}>
+                        <ScrollView
+                            style={styles.scrollView}
+                            scrollViewProps={{
+                                contentContainerStyle: styles.content,
+                            }}
+                            alwaysShowScrollbars
+                        >
+
+                            <div>
+                                <div style={{ width: '100%', display: 'flex' }}>
+                                    <div style={{ float: 'left', margin: '.75rem 0' }}>
+                                        <Text
+                                            bold
+                                            style={{
+                                                fontSize: 22,
+                                                margin: 12,
+                                                marginBottom: 24,
+                                            }}
+                                        >
+                                            Generic Staking
+                                        </Text>
+                                    </div>
+                                </div>
+                                <ConnectMetamask />
+                                {active ? (
+                                    <div>
+                                        <Text style={[styles.textInput]}>
+                                            {tokenBalance ? (<p>Your GENv3 Balance: {tokenBalance} GEN</p>) : (<p></p>)}
+                                            {stakingTokenBalance ? (<p>Stake token balance: {stakingTokenBalance} LP tokens</p>) : (<p></p>)}
+                                            {stakingAmount ? (<p>Staked amount: {stakingAmount} LP tokens</p>) : (<p></p>)}
+                                            {pendingRewards ? (<p>Claimable rewards: {pendingRewards} GEN</p>) : (<p></p>)}
+                                            <input value={depositAmount} onChange={handleDepositAmountChange} />
+                                            {hasAllowance ? (
+                                                <Button primary disabled={!hasAllowance} onPress={() => deposit()}>Deposit</Button>
+                                            ) : (
+                                                <>
+                                                    <Button primary disabled={hasAllowance} onPress={() => handleApprove()}>Approve</Button>
+                                                    <Button primary disabled={hasAllowance} onPress={() => deposit()}>Deposit</Button>
+                                                </>
+                                            )
+                                            }
+                                            <Button primary onPress={() => withdraw()}>Withdraw stake</Button>
+                                            <Button primary onPress={() => claimRewards()}>Claim rewards</Button>
+                                        </Text>
+                                    </div>
+                                ) : (<></>)}
+                            </div>
+
+                        </ScrollView>
+                    </Panel>
+                </Panel>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-  textInput: {
-    input: {
-        fontSize: '3rem',
-    }
-  },
-  textCenter: {
-    textAlign: 'center',
-  },
-  infoView: {
-    maxWidth: '40rem',
-    width: '100%',
-    margin: 'auto',
-  },
-  background: {
-    flex: 1,
-    backgroundColor: '#008080',
-  },
-  container: {
-    flex: 1,
-    maxWidth: '60rem',
-    minWidth: '20rem',
-    width: '100%',
-    margin: 'auto',
-  },
-  textIndent: {
-    paddingLeft: 16,
-  },
-  listItem: {
-    height: 40,
-    paddingHorizontal: 18,
-  },
-  panel: {
-    flex: 1,
-    padding: 8,
-    marginTop: -4,
-    paddingTop: 12,
-  },
-  zpanel: {
-    flex: 1,
-    padding: 8,
-    marginTop: -4,
-    paddingTop: 12,
-    paddingBottom: 100,
-    marginBottom: 18,
-  },
-  slotpanel: {
-    flex: 1,
-    padding: 8,
-    marginTop: -4,
-    paddingTop: 12,
-    paddingBottom: 100,
-    marginBottom: 18,
-    minHeight: '76.6vw',
-  },
-  cutout: {
-    flexGrow: 1,
-    marginTop: 8,
-  },
-  content: {
-    padding: 16,
-  },
-  scrollView: {
-    flex: 1,
-  },
-  section: {
-    marginBottom: 16,
-  },
-  statusBar: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
+    textInput: {
+        input: {
+            fontSize: '3rem',
+        }
+    },
+    textCenter: {
+        textAlign: 'center',
+    },
+    infoView: {
+        maxWidth: '40rem',
+        width: '100%',
+        margin: 'auto',
+    },
+    background: {
+        flex: 1,
+        backgroundColor: '#008080',
+    },
+    container: {
+        flex: 1,
+        maxWidth: '60rem',
+        minWidth: '20rem',
+        width: '100%',
+        margin: 'auto',
+    },
+    textIndent: {
+        paddingLeft: 16,
+    },
+    listItem: {
+        height: 40,
+        paddingHorizontal: 18,
+    },
+    panel: {
+        flex: 1,
+        padding: 8,
+        marginTop: -4,
+        paddingTop: 12,
+    },
+    zpanel: {
+        flex: 1,
+        padding: 8,
+        marginTop: -4,
+        paddingTop: 12,
+        paddingBottom: 100,
+        marginBottom: 18,
+    },
+    slotpanel: {
+        flex: 1,
+        padding: 8,
+        marginTop: -4,
+        paddingTop: 12,
+        paddingBottom: 100,
+        marginBottom: 18,
+        minHeight: '76.6vw',
+    },
+    cutout: {
+        flexGrow: 1,
+        marginTop: 8,
+    },
+    content: {
+        padding: 16,
+    },
+    scrollView: {
+        flex: 1,
+    },
+    section: {
+        marginBottom: 16,
+    },
+    statusBar: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
 
-    marginTop: 4,
-  },
-  statusBarItem: {
-    paddingHorizontal: 6,
-    height: 32,
-    justifyContent: 'center',
-  },
-  header: {
-    justifyContent: 'center',
-    marginBottom: -4,
-    zIndex: 10,
-  },
-  logo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    top: 2,
-  },
-  logoImage: {
-    position: 'absolute',
-    left: -38,
-    top: -4,
-    height: 32,
-    width: 32,
-    resizeMode: 'cover',
-  },
-  heading: {
-    fontSize: 24,
-    fontStyle: 'italic',
-  },
-  aboutButton: {
-    position: 'absolute',
-    right: 8,
-    height: 40,
-    width: 40,
-  },
-  questionMark: {
-    width: 26,
-    height: 26,
-  },
-  scrollPanel: {
-    zIndex: -1,
-  },
+        marginTop: 4,
+    },
+    statusBarItem: {
+        paddingHorizontal: 6,
+        height: 32,
+        justifyContent: 'center',
+    },
+    header: {
+        justifyContent: 'center',
+        marginBottom: -4,
+        zIndex: 10,
+    },
+    logo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 2,
+    },
+    logoImage: {
+        position: 'absolute',
+        left: -38,
+        top: -4,
+        height: 32,
+        width: 32,
+        resizeMode: 'cover',
+    },
+    heading: {
+        fontSize: 24,
+        fontStyle: 'italic',
+    },
+    aboutButton: {
+        position: 'absolute',
+        right: 8,
+        height: 40,
+        width: 40,
+    },
+    questionMark: {
+        width: 26,
+        height: 26,
+    },
+    scrollPanel: {
+        zIndex: -1,
+    },
 });
 
 export default StakingScreen;
